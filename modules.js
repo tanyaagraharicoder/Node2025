aaa57ca35acd157afddde10438cbee041e261de1
Tanya Agrahari <rg96958227@gmail.com>
2025-11-19 00:37:03 +0530

    modules

---
+++ modules.js
@@ -0,0 +1,14 @@
+// const { log } = require('console');
+// const fs = require('fs');
+// const os= require('os')
+// fs.writeFileSync("dummy.txt", "trying with module" )
+// console.log(os.platform());
+// console.log(os.hostname());
+// console.log(os.cpus());
+console.log("abc")
+console.log(process.cwd());
+console.log(process.pid)
+
+
+
+
