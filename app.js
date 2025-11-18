ee2c7e0274f6f8d34dadc198fb0c76549f861551
Tanya Agrahari <rg96958227@gmail.com>
2025-11-18 22:51:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log(20+20);
+
+// function fruit(item){
+//     console.log("fruit name is "+  item);
+
+// }
+// fruit('apple');
+
+var fs= require('fs');
+fs.writeFileSync("TanyaAgrahari.text", " my name is Tanya");
+
+
+
+
+
