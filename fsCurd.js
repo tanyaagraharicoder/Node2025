9ba374e4fd0b791a8575ccba6b5fea6c05d1ab9a
Tanya Agrahari <rg96958227@gmail.com>
2025-11-23 23:05:42 +0530

    curd with file system

---
+++ fsCurd.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+
+console.log(process.argv[2])
+// fs.writeFileSync("apple.txt", "this is a fruit");
+// const data = fs.readFileSync("apple.txt", "utf-8");
+// console.log(data);
+// fs.appendFileSync("apple.txt", "\nI like apple");
